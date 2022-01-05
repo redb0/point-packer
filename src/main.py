c44cb0c41855c6bee9cd59d967c296e8ac8719cd
Anastasiya Semizorova <semizorova2205@mail.ru>
2022-01-05 17:43:44 +0700

    Рефакторинг

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
-from src.figure import Figure
+from figure import Figure
+from circle import Circle
 from visualization import vizualizate
 
 
 def main():
-    fig = Figure(3)
-    points, max_dist = fig.search_point()
-    mid = ((points[0][0] + points[1][0]) / 2,
-           (points[0][1] + points[1][1]) / 2)
-    vizualizate(fig.points, mid, max_dist / 2)
+    fig = Figure(4)
+    circle = Circle(fig)
+    vizualizate(fig.points, circle.center, circle.radius)
 
 
 if __name__ == "__main__":
