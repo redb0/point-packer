2770d757b1870845b0f9fa20b424ec200e278095
Anastasiya Semizorova <semizorova2205@mail.ru>
2022-01-06 18:51:59 +0700

    Рефакторинг

--- src/visualization.py
+++ src/visualization.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 def vizualizate(points, center, radius):
     """ Визуализация точек и окружности """
-    x = [x for x, y in points]
-    y = [y for x, y in points]
+    x = [x for x, _ in points]
+    y = [y for _, y in points]
     fig, ax = plt.subplots()
     ax.scatter(x, y, c='deeppink')
     circle_patch = plt.Circle(center, radius, fill=False)
