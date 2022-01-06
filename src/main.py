682b72e82e397ffc886d4706dd43c567382d5153
Anastasiya Semizorova <semizorova2205@mail.ru>
2022-01-06 22:20:17 +0700

    Изменены входные параметры алгоритма

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from algorithm import GeneticAlgorithm
 
 
 def main():
-    gen = GeneticAlgorithm(3, 10)
+    gen = GeneticAlgorithm(max_iter=5, num_points=6, num_agents=6)
+    gen.start()
 
 if __name__ == "__main__":
     main()
