7f064e3c900bcf79cbb2a92751eea54431d0709d
Anastasiya Semizorova <semizorova2205@mail.ru>
2022-01-06 22:19:24 +0700

    Алгоритм разбит на методы, добавлен учет родителей в новом поколении

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,4 +1,5 @@
 import random
+from operator import attrgetter
 
 from figure import Figure
 from circle import Circle
@@ -7,55 +8,69 @@ from visualization import vizualizate
 
 class GeneticAlgorithm:
 
-    def __init__(self, num_points, num_agents):
-        self.max_iter = 10
+    def __init__(self, max_iter, num_points, num_agents):
+        self.max_iter = max_iter
         self.num_points = num_points
         self.num_agents = num_agents
+        self.population = []
+        self.circles = []
+
+    def initialization(self):
         self.population = self.get_population()
         self.circles = self.get_circles()
-        self.start()
 
     def get_population(self):
         return [Figure(self.num_points) for _ in range(self.num_agents)]
 
     def get_circles(self):
-        return sorted([Circle(agent) for agent in self.population], key=Circle.calc_area)
+        return sorted([Circle(agent) for agent in self.population], key=attrgetter('area'))
 
     def start(self):
+        self.initialization()
         for _ in range(self.max_iter):
-            # выбираем самых перспективных агентов
-            border = self.num_agents // 2
-            # пересчитываем популяцию и остальные параметры
-            self.circles = self.circles[:border]
-            for circle in self.circles:
-                # после этого ничего не изменяется
-                circle.check_points()
-            self.population = [circle.figure for circle in self.circles]
-            # создаем новое поколение
-            # рандомно выбираем индексы пар для слияния
-            count = len(self.population)
-            indexes = []
-            for i in range(count):
-                indexes.append([random.randint(0, count - 1),
-                                random.randint(0, count - 1)])
-                while indexes[i][0] == indexes[i][1]:
-                    indexes[i][1] = random.randint(0, count - 1)
-            # выбираем сколько генов (точек) будем менять
-            num_genes = self.num_points // 2
-            # какие точки меняем
-            right = random.randint(1, self.num_points - num_genes)
-            # скрещиваем выбранные пары
-            child = []
-            for idx in indexes:
-                parent1 = list(self.population[idx[0]].points)
-                parent2 = list(self.population[idx[1]].points)
-                child.append(parent1[0:right] + parent2[right:])
-                child.append(parent2[0:right] + parent1[right:])
-            self.population = [Figure(self.num_points, points=el) for el in child]
-            # проверяем уникальность расстояний, удаляем неподходящие
-            # в планах делать над ними мутацию, уменьшать координаты
-            self.population = [agent for agent in self.population if agent.check_distance() is True]
-            self.circles = self.get_circles()
+            self.selection()
+            self.crossing()
             for circle in self.circles[:2]:
                 print(circle.area)
                 vizualizate(circle.figure.points, circle.center, circle.radius)
+
+    def selection(self):
+        # выбираем самых перспективных агентов
+        border = self.num_agents // 2
+        # пересчитываем популяцию и остальные параметры
+        self.circles = self.circles[:border]
+        flag = False
+        for circle in self.circles:
+            flag = False
+            while not flag:
+                flag = circle.check_points()
+        self.population = [circle.figure for circle in self.circles]
+
+    def crossing(self):
+        # создаем новое поколение
+        # рандомно выбираем индексы пар для слияния
+        count = len(self.population)
+        indexes = []
+        for i in range(count):
+            indexes.append([random.randint(0, count - 1),
+                            random.randint(0, count - 1)])
+            while indexes[i][0] == indexes[i][1]:
+                indexes[i][1] = random.randint(0, count - 1)
+        # выбираем сколько генов (точек) будем менять
+        num_genes = self.num_points // 2
+        # какие точки меняем
+        right = random.randint(1, self.num_points - num_genes)
+        # скрещиваем выбранные пары
+        child = []
+        parents = list(self.population)
+        for idx in indexes:
+            parent1 = list(self.population[idx[0]].points)
+            parent2 = list(self.population[idx[1]].points)
+            child.append(parent1[0:right] + parent2[right:])
+            child.append(parent2[0:right] + parent1[right:])
+        self.population = [Figure(self.num_points, points=el) for el in child]
+        # проверяем уникальность расстояний, удаляем неподходящие
+        # в планах делать над ними мутацию, уменьшать координаты
+        self.population = [agent for agent in self.population if agent.check_distance() is True]
+        self.population = parents + self.population
+        self.circles = self.get_circles()
