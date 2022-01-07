46672198365f9d01485292ca97a48709019d74f8
Anastasiya Semizorova <semizorova2205@mail.ru>
2022-01-08 04:59:55 +0700

    Изменены входные параметры алгоритма

--- src/main.py
+++ src/main.py
@@ -5,8 +5,11 @@ from algorithm import GeneticAlgorithm
 
 
 def main():
-    gen = GeneticAlgorithm(max_iter=5, num_points=6, num_agents=6)
-    gen.start()
+    gen = GeneticAlgorithm(max_iter=3, num_points=10, num_agents=50)
+    circle = gen.start()
+    if circle:
+        vizualizate(circle.figure.points, circle.center, circle.radius)
+        print(f"Минимальная площадь - {circle.area}")
 
 if __name__ == "__main__":
     main()
