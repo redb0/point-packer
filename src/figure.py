e644f6a57f0c0be0783dc097f277ddd2f9222fae
Vladimir <voronov.volodya2013@yandex.ru>
2022-01-08 17:50:22 +0700

    переделана генерация точек

--- src/figure.py
+++ src/figure.py
@@ -1,6 +1,6 @@
-import itertools
 import random
 import numpy as np
+from itertools import product, combinations
 
 
 class Figure:
@@ -31,18 +31,41 @@ class Figure:
     def generate(self):
         """ Генерирует точки по заданному методу """
         flag = True
+        k = 1
         while flag:
             self.points = self.random_generate()
             flag = self.check_distance()
+            # print('k =', k)
+            k += 1
 
     def random_generate(self):
         """ Генерация точек в рандомном месте в заданном интервале
         :return: массив точек
         """
-        right_border = sum([1, self.num_points * 2])
-        x = np.random.randint(0, right_border, self.num_points)
-        y = np.random.randint(0, right_border, self.num_points)
-        return np.array(list(zip(x, y)))
+        right_border_dist = []
+        for i in range(1, self.num_points + 1):
+            if right_border_dist:
+                d = right_border_dist + [sum(item) for item in combinations(right_border_dist, 2)]
+                while i in d:
+                    i += 1
+                right_border_dist.append(i)
+            else:
+                right_border_dist.append(i)
+        right_border = sum(right_border_dist)
+        # right_border = sum([1, self.num_points * 2])
+        # points = np.zeros((self.num_points, 2))
+        # points = np.full((self.num_points, 2), fill_value=np.nan)
+        all_points = list(product(list(range(right_border)), repeat=2))
+        points_idx = np.random.choice(range(len(all_points)), self.num_points)
+        points = np.array([list(all_points[i]) for i in points_idx])
+        # for i in range(self.num_points):
+        #     x = np.random.randint(0, right_border)
+        #     y = np.random.randint(0, right_border)
+        # x = np.random.randint(0, right_border, self.num_points)
+        # y = np.random.randint(0, right_border, self.num_points)
+        # print(points)
+        # return np.array(list(zip(x, y)))
+        return points
 
     def check_distance(self):
         """ Проверка условия, расстояния между всеми парами точек
@@ -51,7 +74,7 @@ class Figure:
                  False в противном случае
         """
         distances = set()
-        comb = itertools.combinations(self.points, 2)
+        comb = combinations(self.points, 2)
         pairs = []
         for pair in comb:
             distance = np.linalg.norm(pair[0] - pair[1])
@@ -68,7 +91,7 @@ class Figure:
         :return: массив с координатами двух точек и расстояние между ними
         """
         max_dist = 0
-        comb = itertools.combinations(self.points, 2)
+        comb = combinations(self.points, 2)
         for pair in comb:
             distance = np.linalg.norm(pair[0] - pair[1])
             if distance > max_dist:
