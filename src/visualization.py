98016f570c987a2eccfe1a6085d56fe1a86cf754
Vladimir <voronov.volodya2013@yandex.ru>
2022-01-09 16:38:14 +0700

    добавлены равные пропорции в визуализацию

--- src/visualization.py
+++ src/visualization.py
@@ -9,4 +9,7 @@ def vizualizate(points, center, radius):
     ax.scatter(x, y, c='deeppink')
     circle_patch = plt.Circle(center, radius, fill=False)
     ax.add_patch(circle_patch)
+    ax.set_xlim([min(x) - 5, max(x) + 5])
+    ax.set_ylim([min(x) - 5, max(x) + 5])
+    plt.axis('equal')
     plt.show()
